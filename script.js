23a059723d12920a2a88338be2c43cd220dd98e1
XxMaxFateXx <aleksa.kumric@gmail.com>
2021-09-30 12:58:42 +0200

    made error input text red and added error border for email red and removed placeholder text when input has an error

--- script.js
+++ script.js
@@ -12,56 +12,63 @@ function validation(event) {
     let errorPass = document.getElementById("error-pass");
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    
-
-    if (email.match(pattern)) { 
-        errorEmail.innerHTML = ""
-        document.getElementById("war5").style.display = "none"
-    }
-    else { 
-        errorEmail.innerHTML = "Looks like this is not an email"  
-        document.getElementById("war5").style.display = "block"
-    }
-
-    if (name == "") { 
+    if (name == "") {
         errorName.innerHTML = "Name cannot be empty"
         document.getElementById("name").style.border = " 2px solid #FF7979";
         document.getElementById("war").style.display = "block"
+        let err = document.getElementById("name")
+        err.classList.add("error-input")
     }
-    else { 
-        errorName.innerHTML = ""  
+    else {
+        errorName.innerHTML = ""
         document.getElementById("name").style.border = "1px solid #DEDEDE";
         document.getElementById("war").style.display = "none"
     }
 
-    if (lastName == "") { 
+    if (lastName == "") {
         errorLastName.innerHTML = "Last Name cannot be empty"
         document.getElementById("last-name").style.border = " 2px solid #FF7979";
         document.getElementById("war2").style.display = "block"
+        let err = document.getElementById("last-name")
+        err.classList.add("error-input")
     }
-    else { 
-        errorLastName.innerHTML = ""  
+    else {
+        errorLastName.innerHTML = ""
         document.getElementById("last-name").style.border = "1px solid #DEDEDE";
         document.getElementById("war2").style.display = "none"
     }
 
-    if (email == "") { 
+    if (email == "") {
         errorEmail.innerHTML = "Email cannot be empty"
         document.getElementById("email").style.border = " 2px solid #FF7979";
         document.getElementById("war3").style.display = "block"
+        let err = document.getElementById("email")
+        err.classList.add("error-input")
     }
-    else{
-        document.getElementById("email").style.border = "1px solid #DEDEDE";
-        document.getElementById("war3").style.display = "none"
+    else {
+        if (email.match(pattern)) {
+            errorEmail.innerHTML = ""
+            document.getElementById("war3").style.display = "none"
+            document.getElementById("email").style.border = "1px solid #DEDEDE";
+        }
+        else {
+            errorEmail.innerHTML = "Looks like this is not an email"
+            document.getElementById("war3").style.display = "block"
+            document.getElementById("email").style.border = "2px solid #FF7979";
+            let text = document.getElementById("email")
+            text.classList.add("error-input-text")
+        }
     }
 
-    if (pass == "") { 
+    if (pass == "") {
         errorPass.innerHTML = "Password cannot be empty"
         document.getElementById("pass").style.border = " 2px solid #FF7979";
         document.getElementById("war4").style.display = "block"
+        let err = document.getElementById("pass")
+        err.classList.add("error-input")
     }
-    else { 
-        errorPass.innerHTML = ""  
+    else {
+        errorPass.innerHTML = ""
         document.getElementById("pass").style.border = "1px solid #DEDEDE";
         document.getElementById("war4").style.display = "none"
     }
